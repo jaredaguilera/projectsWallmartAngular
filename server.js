a045177b9f2cf265fa109dbcae2bf64b4bb81602
jaaguilerz.everis@bancochile.cl <jaaguilerz.everis@bancochile.cl>
2020-10-07 17:48:52 -0400

    cambio

--- server.js
+++ server.js
@@ -5,5 +5,4 @@ app.use(express.static(__dirname+'/dist/projectsWallmart'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/projectsWallmart/index.html'));
 });
-
 app.listen(process.env.PORT || 8080);
