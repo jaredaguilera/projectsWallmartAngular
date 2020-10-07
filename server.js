3167c69593faecd4dd8229755aec0bad64d47b0a
jaaguilerz.everis@bancochile.cl <jaaguilerz.everis@bancochile.cl>
2020-10-06 21:13:14 -0400

    Revert "updates to deploy to heroku"
    This reverts commit 371f73540e1c6cd88e324e547899f307cb2b03d6.

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/projects-wallmart'));
-app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/projects-wallmart/index.html'));});
-app.listen(process.env.PORT || 8080);
