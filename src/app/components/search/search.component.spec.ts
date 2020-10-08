034d7144ba60622bc7ae6a012a7ec3c91cf23b84
jaaguilerz.everis@bancochile.cl <jaaguilerz.everis@bancochile.cl>
2020-10-07 23:06:30 -0400

    prueba angular

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { stringify } from 'querystring';
 
 import { SearchComponent } from './search.component';
 
@@ -20,6 +21,53 @@ describe('SearchComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    let id :string;
+    let product :
+      {
+        "_id": {
+            "timestamp": 1601689289,
+            "machineIdentifier": 13533348,
+            "processIdentifier": -20233,
+            "counter": 6005007,
+            "date": "2020-10-03T01:41:29.000+0000",
+            "time": 1601689289000,
+            "timeSecond": 1601689289
+        },
+        "id": 1,
+        "brand": "ooy eqrceli",
+        "description": "rlñlw brhrka",
+        "image": "www.lider.cl/catalogo/images/whiteLineIcon.svg",
+        "price": 498724.0,
+        "priceLowered": 249362.0
+    }
+    id='1';
+    component.buscar(id)
+    expect(component.buscar(id)).toEqual(product);
   });
+
+  it('should create', () => {
+    let id :string;
+    let product :
+      {
+        "_id": {
+            "timestamp": 1601689289,
+            "machineIdentifier": 13533348,
+            "processIdentifier": -20233,
+            "counter": 6005007,
+            "date": "2020-10-03T01:41:29.000+0000",
+            "time": 1601689289000,
+            "timeSecond": 1601689289
+        },
+        "id": 1,
+        "brand": "ooy eqrceli",
+        "description": "rlñlw brhrka",
+        "image": "www.lider.cl/catalogo/images/whiteLineIcon.svg",
+        "price": 498724.0,
+        "priceLowered": 249362.0
+    }
+    id='1';
+    component.buscar(id)
+    expect(component.buscar(id)).toEqual(product);
+  });
+  
 });
